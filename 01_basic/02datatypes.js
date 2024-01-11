8b82f9f75836c42c0abef576edfafc27bb5e9589
Satyam Rathore <136310276+SRathoreVS@users.noreply.github.com>
2024-01-11 07:06:52 +0000

    datatype

---
+++ 01_basic/02datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code in new version 
+
+// alert(3+3) , we are using NodeJs not browser , it will throw error 
+
+let name = "satyam";
+let age = 24 ;
+let isLoggedIn = true ;
+
+// number => 2 to pwr 53
+// bigInt
+// string => "" '' `` 
+// boolean => true false
+// null => standalone value not 0
+// undefined => 
+// symbol => to find the uniqueness
+
+// object
+
+console.log(typeof age);        //number
+console.log(typeof null);      //object
+console.log(typeof undefined);//undefined
