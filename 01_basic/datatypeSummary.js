b03d7871c6431d6b7b8872b26fe69a8aadbf670e
Satyam Rathore <136310276+SRathoreVS@users.noreply.github.com>
2024-01-15 05:05:08 +0000

    STACK & HEAP

--- 01_basic/datatypeSummary.js
+++ 01_basic/datatypeSummary.js
@@ -44,4 +44,33 @@ console.log(typeof bigNum); // BigInt
 console.log(typeof scoreValue); // number
 console.log(typeof outsideTemp); // Object (as declared as null)
 
+//________________________________________________
+//Memory : Stack(primitive) and Heap(non-primitive)
 
+//STACK(primitive) : we get a copy of orignal value and have change in copy type .
+
+//HEAP(non-primitive) : we get the reference of the orignal value , and so any changes made is changed in orignal value.
+
+//_______________________
+//STACK egs....
+
+let myYoutubeMain = "satyamchelseadotcom";
+let anothername = myYoutubeMain ;
+anothername = "anothername" ;
+
+console.log(myYoutubeMain); //even after changing the value by allocating it in another variable and changing the variables value , the orignal variable will still contain the intial value(orignal value) from the start . therefore ,,,,, // satyamchelseadotcom 
+
+console.log(anothername); // anothername .
+//_______________________
+//HEAP egs....
+
+let user1 = {
+    email : "satyamchelsea123@gmail.com",
+    upi : "user@ybl"
+}
+
+let user2 = user1
+user2.upi = "user2@ybl" ;
+
+console.log(user1.upi); // user2@ybl
+console.log(user2.upi); // user2@ybl
